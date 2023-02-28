b543ebdac3b6957970fa21cd870f3a83bb986d13
elPytel <jaroslav.korner1@gmail.com>
2023-02-28 12:56:34 +0100

    add files

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 ## Cvičení
 1 povolená absence.
 
+## Přednášky
+1. [[ME_UZO_prezentacePřednášky_01.pdf]]
+2. [[ME_UZO_prezentacePřednášky_02.pdf]]
+3. [[ME_UZO_prezentacePřednášky_03.pdf]]
+
 ## Zkouška
 Rozhodnutí známky 1-3.
 
---
+++ cviceni/cv02-init.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+plt.ion()
+clear = lambda: os.system('cls')
+clear()
+plt.close('all')
+
+cap = cv2.VideoCapture('cv02_hrnecek.mp4')
+
+while True:
+    ret, bgr = cap.read()
+    if not ret:
+        break
+    #hsv = cv2.cvtColor(bgr, cv2.COLOR_RGB2HSV)
+    #hist, b = np.histogram(hsv[:,:,0], 256, (0, 256))
+    x1 = 100
+    y1 = 100
+    x2 = 200
+    y2 = 200
+    cv2.rectangle(bgr, (x1, y1), (x2, y2), (0, 255, 0))
+    cv2.imshow('Image', bgr)
+    key = 0xFF & cv2.waitKey(30)
+    if key == 27:
+        break
+    
+cv2.destroyAllWindows()
