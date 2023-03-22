d37b648408fadcdc6f13e197c6ea3f735d045731
elPytel <jaroslav.korner1@gmail.com>
2023-03-22 11:16:34 +0100

    uprava task01

--- cviceni/cv04/task_01.py
+++ cviceni/cv04/task_01.py
@@ -12,6 +12,7 @@ porucha = "cv04_f0"
 etalon = "cv04_e0"
 img_format = ".bmp"
 
+VERBOSE = True
 
 if __name__ == '__main__':
     plt.ion()
@@ -19,15 +20,18 @@ if __name__ == '__main__':
     plt.close('all')
 
     for i in range(1,3):
-        #print(i)
         picture_name = porucha + str(i) + img_format
         etalon_name = etalon + str(i) + img_format
+        if VERBOSE:
+            print("Picture: ", picture_name)
+            print("Etalon: ", etalon_name)
+            print("")
         picture_bgr = cv2.imread(picture_name)
         picture_rgb = cv2.cvtColor(picture_bgr, cv2.COLOR_BGR2RGB)
         etalon_bgr = cv2.imread(etalon_name)
         etalon_rgb = cv2.cvtColor(etalon_bgr, cv2.COLOR_BGR2RGB)
         img = intenzity_corecton(picture_rgb, etalon_rgb)
-        titles = ["Old", "Corected"]
-        plot_imgs([picture_rgb, img], titles)
+        titles = ["Old", "Etalon", "Corected"]
+        plot_imgs([picture_rgb, etalon_rgb, img], titles)
 
 # END
