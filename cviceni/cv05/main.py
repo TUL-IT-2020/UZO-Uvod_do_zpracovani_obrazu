68d0f5b4471a696f13ad685aaa87a4a62c631836
elPytel <jaroslav.korner1@gmail.com>
2023-03-29 11:53:45 +0200

    task B

--- cviceni/cv05/main.py
+++ cviceni/cv05/main.py
@@ -40,8 +40,29 @@ def task_a(img, kernel_size = 3):
         cbars=[False, True, False, True]
     )
 
-def task_b(img):
+def rotation_mask(img, kernel_size = 3):
     pass
+
+def task_b(img):
+    img_bgr = cv2.imread(img)
+    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
+
+    # get spectrum
+    spectrum = np.fft.fftshift(np.fft.fft2(img_gray))
+
+    # apply filters
+    img_filtered = rotation_mask(img_gray)
+    spectrum_filtered = np.fft.fftshift(np.fft.fft2(img_filtered))
+
+    # plot
+    plot_imgs(
+        [img_rgb, amp_spec(spectrum), img_filtered, amp_spec(spectrum_filtered)],
+        ["Robot", "FFT", "Filtered", "Filtered FFT"],
+        2,
+        cmaps=['gray', 'turbo', 'gray', 'turbo'],
+        cbars=[False, True, False, True]
+    )
 
 def median(img, kernel_size = 3):
     """ Implements 2D median filter """
