ddec0148fad50fc6a8be0a9f41096814e56c0346
Jaroslav Körner <jaroslav.korner1@gmail.com>
2023-04-25 16:57:45 +0200

    přidání debugu do cv08

--- cviceni/cv08/main.py
+++ cviceni/cv08/main.py
@@ -34,6 +34,8 @@ from my_libs.colors import *
 from my_libs.img.functional import *
 from my_libs.img.processing import *
 
+DEBUG = False
+
 img_black = "cv08_im1.bmp"
 img_red = "cv08_im2.bmp"
 
@@ -47,31 +49,32 @@ if __name__ == "__main__":
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    """
-    plt.figure("Histogram")
-    plt.hist(img_gray.ravel(), bins=256, range=(0, 256))
-    plt.waitforbuttonpress()
-    plt.close()
-    """
+    if DEBUG:
+        plt.figure("Histogram")
+        plt.hist(img_gray.ravel(), bins=256, range=(0, 256))
+        plt.waitforbuttonpress()
+        plt.close()
     img_gray = segmentate(img_gray, 80, invert=True)
     radius = 6
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (radius, radius))
-    print("Kernel: \n", kernel)
+    if DEBUG:
+        print("Kernel: \n", kernel)
     img_morphed = cv2.morphologyEx(img_gray, cv2.MORPH_OPEN, kernel)
 
     # inverze kvuli barveni objektu
     img_inverted = cv2.bitwise_not(img_morphed)
     colored, numbers = color_objects(img_inverted)
-    print("Numbers:", numbers)
-    plt.figure("Colored objects")
-    plt.imshow(colored)
-    plt.waitforbuttonpress()
-    plt.close()
+    if DEBUG:
+        print("Numbers:", numbers)
+        plt.figure("Colored objects")
+        plt.imshow(colored)
+        plt.waitforbuttonpress()
+        plt.close()
 
     centers = calculate_centers_of_objects(colored, numbers)
 
     plot_imgs([img, img_gray, img_morphed], 
-              ["img", "gray", "morhed"], 2, 
+              ["img", "gray", "morphed"], 2, 
               cmaps=[None, "gray", "gray"],  
               window_name="Image with black dots"
     )
@@ -82,12 +85,13 @@ if __name__ == "__main__":
 
     r_color_space = img_to_r(img)
     r_color_space = normalize(r_color_space)
-    """
-    plt.figure("Histogram")
-    plt.hist(r_color_space.ravel(), bins=256, range=(0, 256))
-    plt.waitforbuttonpress()
-    plt.close()
-    """
+    
+    if DEBUG:
+        plt.figure("Histogram")
+        plt.hist(r_color_space.ravel(), bins=256, range=(0, 256))
+        plt.waitforbuttonpress()
+        plt.close()
+
     r_segmented = segmentate(r_color_space, 100, invert=False)
 
     plot_imgs([img, r_color_space, r_segmented], 
